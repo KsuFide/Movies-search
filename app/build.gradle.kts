bc0d6193c108e78df1c5657f18ec6368caca25ac
Ksenia Fedyaeva <161697268+KsuFide@users.noreply.github.com>
2024-12-17 19:11:25 +0800

    Module 20

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.moviesearch"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.moviesearch"
--- app/src/main/java/com/example/moviesearch/MainActivity.kt
+++ app/src/main/java/com/example/moviesearch/MainActivity.kt
@@ -1,20 +1,66 @@
 package com.example.moviesearch
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Вызываем метод для инициализации кнопок
+        initMenuButtons()
+    }
+
+    private fun initMenuButtons() {
+        // Находим кнопку по id
+        val button1 = findViewById<Button>(R.id.button1)
+        // Устанавливаем обработчик нажатий
+        button1.setOnClickListener {
+            // Показываем Toast-сообщение
+            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+        }
+
+        // Находим кнопку по id
+        val button2 = findViewById<Button>(R.id.button2)
+        // Устанавливаем обработчик нажатий
+        button2.setOnClickListener {
+            // Показываем Toast-сообщение
+            Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+
+
+            // Находим кнопку по id
+            val button3 = findViewById<Button>(R.id.button3)
+            // Устанавливаем обработчик нажатий
+            button3.setOnClickListener {
+                // Показываем Toast-сообщение
+                Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
+
+            }
+
+            // Находим кнопку по id
+            val button4 = findViewById<Button>(R.id.button4)
+            // Устанавливаем обработчик нажатий
+            button4.setOnClickListener {
+                // Показываем Toast-сообщение
+                Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+
+            }
+
+            // Находим кнопку по id
+            val button5 = findViewById<Button>(R.id.button5)
+            // Устанавливаем обработчик нажатий
+            button5.setOnClickListener {
+                // Показываем Toast-сообщение
+                Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+
+            }
         }
     }
 }
