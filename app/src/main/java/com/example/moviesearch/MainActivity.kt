0e9c113aeb4d3e1add8f2454c10d296d2205d894
Ksenia Fedyaeva <161697268+KsuFide@users.noreply.github.com>
2025-03-11 14:19:39 +0800

    Перенесла список фильмов в отдельный файл(Data.kt) Загрузила список в RV

--- app/src/main/java/com/example/moviesearch/MainActivity.kt
+++ app/src/main/java/com/example/moviesearch/MainActivity.kt
@@ -10,108 +10,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.moviesearch.Data.films
 import com.example.moviesearch.databinding.ActivityMainBinding
-import com.google.android.material.appbar.MaterialToolbar
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var filmsAdapter: FilmListRecyclerAdapter
 
-    //Создаём список фильмов
-    private val filmsDataBase = listOf(
-        Film(
-            "Парк Юрского периода",
-            R.drawable.jurassic_park,
-            "Ученые клонируют динозавров для тематического парка, но что-то идет не так..."
-        ),
-        Film(
-            "Человек-паук",
-            R.drawable.spider_man,
-            "Питер Паркер обретает суперспособности после укуса радиоактивного паука"
-        ),
-        Film(
-            "Мумия",
-            R.drawable.the_mummy,
-            "Археологи пробуждают древнее проклятие во время раскопок в Египте"
-        ),
-        Film(
-            "Человек-волк",
-            R.drawable.wolf_man,
-            "Проклятие превращает человека в оборотня во время полнолуния"
-        ),
-        Film(
-            "Флэш",
-            R.drawable.flash,
-            "Барри Аллен получает сверхчеловеческую скорость после аварии с ускорителем частиц"
-        ),
-        Film(
-            "Мстители",
-            R.drawable.avengers,
-            "Команда супергероев объединяется, чтобы спасти Землю от Локи и инопланетного вторжения"
-        ),
-        Film(
-            "1+1",
-            R.drawable.the_intouchables,
-            "Неожиданная дружба между богатым инвалидом и уличным парнем из трущоб"
-        ),
-        Film(
-            "Бойцовский клуб",
-            R.drawable.fight_club,
-            "Офисный работник и мыловар создают подпольную организацию для психологической разрядки"
-        ),
-        Film(
-            "Зелёная миля",
-            R.drawable.green_mile,
-            "История надзирателя смертников и заключённого с необычным даром"
-        ),
-        Film(
-            "Зелёная книга",
-            R.drawable.green_book,
-            "Путешествие афроамериканского пианиста и итальянского водителя по югу США 1960-х"
-        ),
-        Film(
-            "Форрест Гамп",
-            R.drawable.forrest_gump,
-            "Простой парень становится невольным участником ключевых событий американской истории"
-        ),
-        Film(
-            "Гарри Поттер",
-            R.drawable.harry_potter,
-            "Мальчик-сирота узнаёт, что он волшебник, и поступает в школу Хогвартс"
-        ),
-        Film(
-            "Властелин колец",
-            R.drawable.lord_of_rings,
-            "Эпическое путешествие хоббита Фродо для уничтожения Кольца Всевластия"
-        ),
-        Film(
-            "Интерстеллар",
-            R.drawable.interstellar,
-            "Космическая экспедиция сквозь червоточину в поисках нового дома для человечества"
-        ),
-        Film(
-            "Остров проклятых",
-            R.drawable.shutter_island,
-            "Детектив расследует исчезновение пациентки психиатрической больницы на отдалённом острове"
-        ),
-        Film(
-            "Джентльмены",
-            R.drawable.gentlemen,
-            "Криминальный босс пытается продать свой наркобизнес, но сталкивается с проблемами"
-        ),
-        Film(
-            "История игрушек",
-            R.drawable.toy_story,
-            "Игрушки оживают, когда людей нет рядом, и переживают удивительные приключения"
-        ),
-        Film(
-            "Побег из Шоушенка",
-            R.drawable.shawshank_redemption,
-            "Несправедливо осуждённый банкир Энди Дюфрейн 20 лет планирует побег из тюрьмы, сохраняя надежду и человечность"
-        )
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -121,10 +27,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.mainRecycler.apply {
-            layoutManager = LinearLayoutManager(this@MainActivity)
+
+            // Инициализируем наш адаптер в конструктор передаём анонимно инициализированный интерфейс
+            filmsAdapter =
+                FilmListRecyclerAdapter(object : FilmListRecyclerAdapter.OnItemClickListener {
+                    override fun click(film: Film) {
+
+                    }
+                })
+
+            // Присваиваем адаптер
             adapter = filmsAdapter
-            addItemDecoration(TopSpacingItemDecoration(8))
+            // Присвоим layoutManager
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            // Применяем декоратор для отступов
+            val decoration = TopSpacingItemDecoration(8)
+            addItemDecoration(decoration)
         }
+        // Кладём нашу БД в RV
+        filmsAdapter.addItems(films)
 
 
         // Инициализация BottomNavigationView
